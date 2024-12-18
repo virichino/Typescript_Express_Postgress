a274d3ee8e910db87b4e6e753eefcf123ff10553
Carlos Tito <viri_nxg@hotmail.com>
2024-12-18 01:57:38 -0400

    Deleting dist folder, and including validator for the json payload when doing an API request for the endpoint to register a user

--- dist/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const router = (0, express_1.Router)();
-router.get('/', userController_1.getAllUsers);
-router.post('/', userController_1.addUser);
-exports.default = router;
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,33 @@
 import { Router } from 'express';
+import { check } from 'express-validator';
+import { validate } from '../middleware/validate';
 import { register, login } from '../controllers/authController';
 
 const router = Router();
 
-router.post('/register', register); // Registro de usuario
-router.post('/login', login);       // Inicio de sesión
+router.post(
+  '/register',
+  [
+    check('name')
+    .notEmpty().withMessage('Name is required')
+    .isLength({ max: 10}).withMessage('Name must be les than 10 characters long'),
+    check('email').isEmail().withMessage('Invalid email format'),
+    check('password')
+      .isLength({ min: 6 })
+      .withMessage('Password must be at least 6 characters long'),
+    validate, // Middleware de validación
+  ],
+  register
+);
+
+router.post(
+  '/login',
+  [
+    check('email').isEmail().withMessage('Invalid email format'),
+    check('password').notEmpty().withMessage('Password is required'),
+    validate, // Middleware de validación
+  ],
+  login
+);
 
 export default router;
